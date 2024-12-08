a1bd69fbfdf72371a39c27f8aeba3da787a48b5e
vaibhavjoshi2000 <vaibhavjoshibawa@gmail.com>
2024-12-08 10:14:44 +0530

    Add project files.

---
+++ SimpleInterest/Controllers/SimpleInterestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleInterest.Models;
+namespace SimpleInterest.Controllers
+{
+    public class SimpleInterestController : Controller
+    {
+        // GET: SimpleInterest
+        public ActionResult DisplaySimpleInterest()
+        {
+           SimpleInterestModel obj = new SimpleInterestModel();
+           return View(obj);
+          
+        }
+
+        [HttpPost]
+        public ActionResult CalculateSimpleInterest(SimpleInterestModel model)
+        {
+            // Validate the model
+            if (!ModelState.IsValid)
+            {
+                // Return the same view with validation errors
+                return View("DisplaySimpleInterest", model);
+            }
+
+            //Calculate Simple Interst
+            model.SimpleInterest = (model.Principal * model.Amount * model.Year) / 100;
+            return View("DisplaySimpleInterest", model);//"DisplaySimpleInterest": This explicitly tells ASP.NET MVC which Razor view to render, irrespective of the action method name & in this case it will render DisplaySimpleInterest view.
+        }
+    }
+}
---
+++ SimpleInterest/Models/SI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleInterest.Models
+{
+    public class SimpleInterestModel
+    {
+        [Required(ErrorMessage = "Principal amount is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Principal amount must be greater than zero.")]
+        public decimal? Principal { get; set; }
+        [Required(ErrorMessage = "Rate of interest is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Rate of interest must be greater than zero.")]
+        public decimal? Amount {  get; set; }   //Here Amount refers to Rate
+        [Required(ErrorMessage = "Number of years is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Number of years must be at least 1.")]
+        public int? Year { get; set; }
+        [Required(ErrorMessage = "Simple interest is required.")]
+        public decimal? SimpleInterest { get; set; }
+    }
+}
